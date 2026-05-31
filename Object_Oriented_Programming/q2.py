89738f1d39cf43f120e7a1b1a7dcf50a04eac7aa
Md sahil <mdsahil880999@gmail.com>
2026-05-31 16:26:45 +0530

    Restore Student class and example usage
    deleted unnecessary code

--- Object_Oriented_Programming/q2.py
+++ Object_Oriented_Programming/q2.py
@@ -1,17 +1,13 @@
-# class Student:
-#     def __init__(self, name, marks):
-#         self.name = name
-#         self.marks = marks
-#     def grade(self):
-#         if self.marks >= 90:
-#             return "A"
-#         elif self.marks >= 75:
-#             return "B"
-#         else:
-#             return "C"
-# s1 = Student("Riya", 88)
-# print(s1.name, "got grade", s1.grade())
-
-# from Object_Oriented_Programming import car
-
-from q3 import Vehicle
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+    def grade(self):
+        if self.marks >= 90:
+            return "A"
+        elif self.marks >= 75:
+            return "B"
+        else:
+            return "C"
+s1 = Student("Riya", 88)
+print(s1.name, "got grade", s1.grade())
